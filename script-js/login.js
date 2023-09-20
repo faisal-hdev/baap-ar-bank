21f7cba1469ea69caf63754b5272bd5434f48016
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-09-20 23:33:06 +0600

    second commit

--- script-js/login.js
+++ script-js/login.js
@@ -4,16 +4,18 @@ document.getElementById("btn-submit").addEventListener("click", function () {
   // always remember to use .value to get text from an input filed
   const emailField = document.getElementById("user-email");
   const email = emailField.value;
-  const passField = document.getElementById("user-password");
-  const password = passField.value;
-  console.log(email, password);
-
-  if (email === "faisal@gmail.com" && password === "12345") {
-    console.log("btn clicked");
-    window.location.href = "../bank.html";
-  }
   // Step-3: get password
   // 3.1 set id on the html element
   // 3.2 get the element
   // 3.3 get the value from the element
+  const passField = document.getElementById("user-password");
+  const password = passField.value;
+
+  // DANGER : Do not email verify email password on the client side
+  // Step-4: verify email and password
+  if (email === "faisal@gmail.com" && password === "12345") {
+    window.location.href = "../bank.html";
+  } else {
+    alert("Your password is wrong");
+  }
 });
